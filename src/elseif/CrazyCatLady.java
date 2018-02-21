95431f1c6659a3acc96795ee9979719399f51716
Laplante.Ewan <Laplante.Ewan@SEMS-259-30.SEMS259.LAB>
2018-02-21 14:57:16 -0800

    Cat fizzBuzz

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,26 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+String Cats = JOptionPane.showInputDialog("How many cats do you have??????");
 		// 2. Convert their answer into an int
-
+int cats = Integer.parseInt(Cats);
 		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
-
+if(cats >= 3) {
+	JOptionPane.showMessageDialog(null, "You're a crazy cat lady");
+}
 		// 4. If they have 3 or less, call the method below to show them a cat video
-
+if(cats < 3 && cats >0) {
+	playVideo("https://www.youtube.com/watch?v=XyNlqQId-nk");
+}
 		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if(cats == 0) {
+			playVideo("https://www.youtube.com/watch?v=W_juM14WHNQ");
+		}
 	}
 
 	static void playVideo(String videoURL) {
